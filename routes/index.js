f0d928d7f794ea93ab30f66241cca22d5a45f97e
Rachel <rsluk@ucsd.edu>
2020-02-06 13:41:03 -0800

    Lab 5

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,22 @@ var data = require('../data.json');
 
 exports.view = function(request, response){
 	console.log(data);
-	response.render('index');
+        response.render('index', {
+            "parents":[
+            {"name":"Friend1",
+            "image": "images/http://lorempixel.com/500/500/people",
+            "description":"Description here 1"
+            },
+            {
+            "name":"Friend2",
+            "image":"images/http://lorempixel.com/500/500/people",
+            "description":"Description here 2"
+            },
+            {
+            "name":"Friend3",
+            "image":"images/http://lorempixel.com/500/500/people",
+            "description":"Description here 3"
+            },
+            ]
+            });
 };
